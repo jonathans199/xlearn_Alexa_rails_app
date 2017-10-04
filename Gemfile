520d370b84ed22ee1610a4033726b499c63b8f20
Oksana Hoeckele <oksanaskupa@gmail.com>
2017-10-04 13:46:55 -0400

    Add Facebook login

--- Gemfile
+++ Gemfile
@@ -11,6 +11,10 @@ gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
+gem 'devise', '~> 4.3'
+
+gem 'omniauth-facebook', '~> 4.0.0'
+
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
@@ -32,6 +36,7 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'dotenv-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171003193904) do
+ActiveRecord::Schema.define(version: 20171003223604) do
 
   create_table "courses", force: :cascade do |t|
     t.string "course_name"
@@ -25,6 +25,10 @@ ActiveRecord::Schema.define(version: 20171003193904) do
     t.string "course"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.text "image"
   end
 
 end
