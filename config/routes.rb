597d11c4cb7bd905134b2d72ac8e1c7db8806bc9
Jonathan Sanchez <jso1919@gmail.com>
2017-10-09 17:46:35 -0400

    finished overall template integration

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
  get 'auth/:provider/callback', to: 'sessions#create'
  get 'auth/failure', to: redirect('/')
  # get 'auth/facebook', to: redirect
+ get '/signout', to: 'sessions#destroy'
  delete '/signout', to: 'sessions#destroy'
 
   resources :courses
   resources :users
   root 'static_pages#home'
+
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
