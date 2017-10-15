cd3c1362f4357e4b06f6ac66d59d0b98a2e72e21
Oksana Hoeckele <oksanaskupa@gmail.com>
2017-10-15 15:11:16 -0400

    adding seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,14 +56,14 @@ end
 #
 #
 #
-#
-# # Course.create(course_name: "German", description: "Far far away, behind the word mountains, far from the countries Vokalia..")
-# Course.create(course_name: "Ukrainian", description: "The official language of Ukraine is Ukrainian, an East Slavic language which is the native language of 67.5% of Ukraine's population.")
-# Course.create(course_name: "Portuguese", description: "Portuguese is a western Romance language and the sole official language of Portugal, Brazil, Cape Verde, Guinea-Bissau, Mozambique, Angola, and São")
-# Course.create(course_name: "German", description: "German language, German Deutsch, official language of both Germany and Austria and one of the three official languages of Switzerland.")
-# Course.create(course_name: "French", description: "French is a Romance language of the Indo-European family. It descended from the Vulgar Latin of the Roman Empire, as did all Romance languages.")
-# # Course.create(course_name: "Ukrainian", description: "Far far away, behind the word mountains, far from the countries Vokalia..")
-# #Course.create(course_name: "Portuguese", description: "Far far away, behind the word mountains, far from the countries Vokalia..")
-# Course.create(course_name: "Italian", description: "Italian is a Romance language. By most measures, Italian, together with Sardinian, is the closest to Latin of the Romance languages.")
-#
-# puts "Seed file completed! :)"
+
+Course.create(course_name: "German", description: "Far far away, behind the word mountains, far from the countries Vokalia..")
+Course.create(course_name: "Ukrainian", description: "The official language of Ukraine is Ukrainian, an East Slavic language which is the native language of 67.5% of Ukraine's population.")
+Course.create(course_name: "Portuguese", description: "Portuguese is a western Romance language and the sole official language of Portugal, Brazil, Cape Verde, Guinea-Bissau, Mozambique, Angola, and São")
+Course.create(course_name: "German", description: "German language, German Deutsch, official language of both Germany and Austria and one of the three official languages of Switzerland.")
+Course.create(course_name: "French", description: "French is a Romance language of the Indo-European family. It descended from the Vulgar Latin of the Roman Empire, as did all Romance languages.")
+Course.create(course_name: "Ukrainian", description: "Far far away, behind the word mountains, far from the countries Vokalia..")
+Course.create(course_name: "Portuguese", description: "Far far away, behind the word mountains, far from the countries Vokalia..")
+Course.create(course_name: "Italian", description: "Italian is a Romance language. By most measures, Italian, together with Sardinian, is the closest to Latin of the Romance languages.")
+
+puts "Seed file completed! :)"
